785b109e0570b6a1387e57e073096c1ed95bba45
ParkSJ00 <youi1213@naver.com>
2022-08-30 17:35:14 +0900

    SQL데이터저장

---
+++ src/main/java/com/kh/myapp3/domain/dao/PreparedStatementCreatorImpl.java
@@ -0,0 +1,4 @@
+package com.kh.myapp3.domain.dao;
+
+public interface PreparedStatementCreatorImpl {
+}
---
+++ src/main/java/com/kh/myapp3/web/form/SaveForm.java
@@ -0,0 +1,17 @@
+package com.kh.myapp3.web.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class SaveForm {
+  private String pname; // 상품명 PNAME	VARCHAR2(30 BYTE)	Yes		2
+  private Integer quantity; //수량 QUANTITY	NUMBER(10,0)	Yes		3
+  private Integer price; //가격 PRICE	NUMBER(10,0)	Yes		4
+
+}
